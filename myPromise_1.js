4aace2aa9d99bcf7f20b25bb250191017cc07b01
pan227 <xienr106@gmail.com>
2020-09-26 19:21:12 -0700

    enhance promise

--- myPromise_1.js
+++ myPromise_1.js
@@ -1,3 +1,8 @@
+// https://javascript.info/promise-chaining
+// 感觉fulfill没有用上
+// 如果有5个then, 第一个即时执行，直接跳到fulfilled 状态
+// 如果第二个是async, 就会进入pending 状态，把后面的都装进去observerList, 等到第一个resolved了，就开始执行array里面的内容
+// 如果有不连在一起的then 5 个， 这一层observerList 会有 5 个function
 class MyPromise1 {
     constructor(run) {
         this.status = "pending";
@@ -21,7 +26,8 @@ class MyPromise1 {
                     this.observerList.push(val => {
                         const result = callback(val);
                         if (result instanceof MyPromise1) {
-                            result.then(resolve);
+                            result.then(resolve); // 相当于 Promise.then(val => resolve(val))
+                            // then 的返回是Promise类型，但then 里面回调函数的返回类型不一定是Promise
                         } else {
                             resolve(result);
                         }
@@ -33,20 +39,15 @@ class MyPromise1 {
                     if (result instanceof MyPromise1) {
                         result.then(resolve);
                     } else {
-                        resolve(result);
+                        resolve(this.data);
                     }
                 })
         }
     }
 }
 
-const p = new MyPromise1(resolve => {
-    setTimeout(() => {
-        resolve("hello world");
-        resolve("hello world2"); // 不好使了
-    }, 1000);
+const p = new MyPromise1((resolve, reject) => {
+    resolve("sss");
 });
-
-p.then(value => value + "dpf")
-    .then(value => value.toUpperCase())
-    .then(console.log);
+p.then(v => v).then(v => new MyPromise1(resolve => setTimeout(() => resolve(v + "dpf")), 1000)).then(v => console.log(v));
+//v => (console.log('xxx'), v)
